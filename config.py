27d3b7a94515af0c2a65122bd5fd36ab213cdf5f
Young Ju Park <parkyngj@gmail.com>
2017-01-29 21:38:06 -0800

    Define list of OpenID providers

--- config.py
+++ config.py
@@ -2,3 +2,11 @@
 WTF_CSRF_ENABLED = True
 # Create a cryptographic token that is used to validate a form
 SECRET_KEY = 'you-will-never-guess'
+
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
