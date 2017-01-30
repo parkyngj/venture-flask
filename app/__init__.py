afc6612252a361e657800f73b0600cd9ed4f53c9
Young Ju Park <parkyngj@gmail.com>
2017-01-29 21:05:22 -0800

    Sets up config usage in app in app init

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,10 @@
 Create the application object of class Flask, and then imports the views module.
 '''
 from flask import Flask
+
 app = Flask(__name__)
+
+# Tell Flask to read and use our config file
+app.config.from_object('config')
+
 from app import views
