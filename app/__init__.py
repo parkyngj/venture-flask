1f81a2decc078ed9b6a5216c90e45a86adc45aea
Young Ju Park <parkyngj@gmail.com>
2017-01-29 21:57:13 -0800

    Create db object and import models

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@
 Create the application object of class Flask, and then imports the views module.
 '''
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Tell Flask to read and use our config file
 app.config.from_object('config')
 
-from app import views
+# Create db object that will be our database
+db = SQLAlchemy(app)
+
+from app import views, models
