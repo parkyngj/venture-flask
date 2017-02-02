d376b3d1bd5252bd2c9309777d8fc97a5a3ee455
Young Ju Park <parkyngj@gmail.com>
2017-02-02 15:39:13 -0800

    Adds post relation for User

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     # Tells Python how to print objects of this class
     def __repr__(self):
